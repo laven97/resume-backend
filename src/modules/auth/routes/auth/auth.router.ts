47cc04a977946061bd8898079c3a5ed5d24b83ce
laven97 <poleschucke@gmail.com>
2026-09-15 12:01:02 +0200

    refactor(auth): simplify route definitions by removing validation middleware refactor(parfume): remove validation middleware from create and update routes refactor: update TypeScript configuration to use node16 module and resolution

--- src/modules/auth/routes/auth/auth.router.ts
+++ src/modules/auth/routes/auth/auth.router.ts
@@ -1,31 +1,16 @@
-import { Router } from "express";
-import { celebrate } from "celebrate";
+import { Router } from 'express';
 
-import { AuthValidation } from "../../validation/auth.validator";
-import { authController } from "../../controller/auth/auth.controller";
-import { authMiddleware } from "../../../../common/middleware/auth/auth.middleware";
+import { authController } from '../../controller/auth/auth.controller';
+import { authMiddleware } from '../../../../common/middleware/auth/auth.middleware';
 
 const router = Router();
 
-router.post(
-  "/sign-up",
-  celebrate(AuthValidation.registerUserSchema),
-  authController.signUp
-);
+router.post('/sign-up', authController.signUp);
 
-router.post(
-  "/sign-in",
-  celebrate(AuthValidation.loginUserSchema),
-  authController.signIn
-);
+router.post('/sign-in', authController.signIn);
 
-router.post(
-  "/logout",
-  authMiddleware.checkAccessToken,
-  celebrate(AuthValidation.logoutSchema),
-  authController.logout
-);
+router.post('/logout', authMiddleware.checkAccessToken, authController.logout);
 
-router.post("refresh", authController.refreshTokens);
+router.post('refresh', authController.refreshTokens);
 
 export const authRouter = router;
--- src/modules/parfume/routes/parfume.router.ts
+++ src/modules/parfume/routes/parfume.router.ts
@@ -1,9 +1,8 @@
-import { celebrate } from 'celebrate';
+
 import { Router } from 'express';
 
 import { authMiddleware } from '../../../common/middleware/auth/auth.middleware';
 import { checkAccess } from '../../../common/middleware/user/roleAuthentication.middleware';
-import { ParfumeValidation } from '../validation/parfume.validation';
 import { parfumeController } from '../controller/parfume.controllers';
 import { commonMiddleware } from '../../../common/middleware/common/common.middleware';
 import { Permissions } from '../../user/types/permision.type';
@@ -16,7 +15,6 @@ router.post(
   '/create',
   authMiddleware.checkAccessToken,
   checkAccess(Permissions.CREATE_PARFUME),
-  celebrate(ParfumeValidation.createParfume),
   parfumeController.createParfume,
 );
 
@@ -25,7 +23,6 @@ router.put(
   authMiddleware.checkAccessToken,
   checkAccess(Permissions.UPDATE_PARFUME),
   commonMiddleware.verifyId,
-  celebrate(ParfumeValidation.updateParfume),
   parfumeController.updateParfumeById,
 );
 
